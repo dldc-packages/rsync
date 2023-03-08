e0caba9613d0309013eb6e634b83a8710ab63ba8
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-03-08 23:09:09 +0100

    Update deno build

--- deno_dist/ZenRsyncErreur.ts
+++ deno_dist/ZenRsyncErreur.ts
@@ -1,3 +1,5 @@
 import { Erreur } from 'https://raw.githubusercontent.com/etienne-dldc/erreur/v2.0.0/deno_dist/mod.ts';
 
-export const ZenRsyncErreur = Erreur.declareMany({});
+export const ZenRsyncErreur = Erreur.declareMany({
+  // TODO
+});
--- deno_dist/mod.ts
+++ deno_dist/mod.ts
@@ -1,3 +1,4 @@
 export * from './apply.ts';
 export * from './diff.ts';
 export * from './prepare.ts';
+export * from './ZenRsyncErreur.ts';
