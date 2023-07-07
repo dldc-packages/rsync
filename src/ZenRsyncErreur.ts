c7f9ee2cf8aa76d4756e9e86e84c0132be074c84
Etienne Deladonchamps <etienne@husprey.com>
2023-07-07 16:11:22 +0200

    Upgrade setup

--- src/ZenRsyncErreur.ts
+++ src/ZenRsyncErreur.ts
@@ -8,13 +8,13 @@ export const ZenRsyncErreur = {
       return err
         .with(provider)
         .withMessage(`Unexpected diff, patch block after ${blockIndex}, but the block is not matched`);
-    }
+    },
   ),
   BlockCountMismatch: ErreurType.define<{ expected: number; actual: number }>(
     'BlockCountMismatch',
     (err, provider, { expected, actual }) => {
       return err.with(provider).withMessage(`Block count mismatch, expected ${expected}, got ${actual}`);
-    }
+    },
   ),
   UnexpectedEof: ErreurType.defineEmpty('UnexpectedEof', (err, provider) => {
     return err.with(provider).withMessage('Unexpected end of file');
